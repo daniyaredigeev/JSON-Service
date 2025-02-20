150293273fe69db2f2b7a610dd50cdbc6393efbd
Daniyar Yedigeyev <dedigeev2@gmail.com>
2025-02-21 02:47:59 +0500

    add readme

---
+++ WebApplication4/README.MD
@@ -0,0 +1,1 @@
+This is a JSON service that returns a random fact, a joke, a random number and a fact about cats, launched via /facts
--- WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722c895b5e06fef10ea6c9b0ec000a8048db7121")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
